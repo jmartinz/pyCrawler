5ac37da422793631afde23c61dcbf4683bcb9fbb
jmartinz <jose.maria.martin.pitto@gmail.com>
2015-06-26 03:43:54 +0200

    Codigo más bonito

---
+++ 10.contratacionE/save.p
@@ -0,0 +1,215 @@
+(i__main__
+Contrato
+(dp1
+S'organo'
+p2
+VSubdirección General de Análisis Presupuestario y Gestión Financiera del Instituto de Mayores y Servicios Sociales 
+p3
+sS'num_expediente'
+p4
+V219/2015
+p5
+sS'Fecha'
+p6
+(dp7
+VF. Adjudicación:
+p8
+V22/05/2015
+p9
+sVPresentación: 
+p10
+S''
+sVF. Formalización:
+p11
+V24/06/2015
+p12
+sVAdj. Provisional: 
+p13
+S''
+sVAdj. Definitiva: 
+p14
+S''
+ssS'tiposContrato'
+p15
+(lp16
+VServicios
+p17
+aVServicios de informática y servicios conexos
+p18
+asS'organoURL'
+p19
+S'https://contrataciondelestado.es/wps/poc?uri=deeplink:perfilContratante&idBp=p6cgeJ%2FgjC4QK2TEfXGy%2BA%3D%3D'
+p20
+sS'estado'
+p21
+VResuelta
+p22
+sS'desc_expediente'
+p23
+VActualización y garantía de las licencias de los productos Oracle del Imserso
+p24
+sS'importe'
+p25
+V177.921,38
+p26
+sb.(i__main__
+Contrato
+(dp1
+S'organo'
+p2
+VGerencia de Atención Sanitaria de Melilla - Instituto Nacional de Gestión Sanitaria
+p3
+sS'num_expediente'
+p4
+VP.A. 3/2014
+p5
+sS'Fecha'
+p6
+(dp7
+VF. Adjudicación:
+p8
+S''
+sVPresentación: 
+p9
+V20/07/2015
+p10
+sVF. Formalización:
+p11
+S''
+sVAdj. Provisional: 
+p12
+S''
+sVAdj. Definitiva: 
+p13
+S''
+ssS'tiposContrato'
+p14
+(lp15
+VSuministros
+p16
+aVAdquisición
+p17
+asS'organoURL'
+p18
+S'https://contrataciondelestado.es/wps/poc?uri=deeplink:perfilContratante&idBp=n%2BtU5Y9ci1o%3D'
+p19
+sS'estado'
+p20
+VPublicada
+p21
+sS'desc_expediente'
+p22
+VSuministro de reactivos y del sistema de información y servicio de mantenimiento del mismo para la realización de determinaciones analíticas automáticas en el laboratorio del Hospital Comarcal de Melilla
+p23
+sS'importe'
+p24
+V3.464.483,55
+p25
+sb.(i__main__
+Contrato
+(dp1
+S'organo'
+p2
+VSecretaría General de la Agencia Española de Medicamentos y Productos Sanitarios
+p3
+sS'num_expediente'
+p4
+V2015/213/00008
+p5
+sS'Fecha'
+p6
+(dp7
+VF. Adjudicación:
+p8
+V24/06/2015
+p9
+sVPresentación: 
+p10
+S''
+sVF. Formalización:
+p11
+S''
+sVAdj. Provisional: 
+p12
+S''
+sVAdj. Definitiva: 
+p13
+S''
+ssS'tiposContrato'
+p14
+(lp15
+VServicios
+p16
+aVServicios de mantenimiento y reparación
+p17
+asS'organoURL'
+p18
+S'https://contrataciondelestado.es/wps/poc?uri=deeplink:perfilContratante&idBp=XqO%2BFjMho9A%3D'
+p19
+sS'estado'
+p20
+VAdjudicada
+p21
+sS'desc_expediente'
+p22
+VServicio de  verificación/calibración y mantenimiento preventivo y correctivo de los equipos Metrhom, Erweka, Hanson y Rudolph Research Analytical instalados en los Laboratorios de la Agencia Española de Medicamentos y Productos Sanitarios
+p23
+sS'importe'
+p24
+V32.542,00
+p25
+sb.(i__main__
+Contrato
+(dp1
+S'organo'
+p2
+VSubdirección General de Análisis Presupuestario y Gestión Financiera del Instituto de Mayores y Servicios Sociales 
+p3
+sS'num_expediente'
+p4
+V219/2015
+p5
+sS'Fecha'
+p6
+(dp7
+VF. Adjudicación:
+p8
+V22/05/2015
+p9
+sVPresentación: 
+p10
+S''
+sVF. Formalización:
+p11
+V24/06/2015
+p12
+sVAdj. Provisional: 
+p13
+S''
+sVAdj. Definitiva: 
+p14
+S''
+ssS'tiposContrato'
+p15
+(lp16
+VServicios
+p17
+aVServicios de informática y servicios conexos
+p18
+asS'organoURL'
+p19
+S'https://contrataciondelestado.es/wps/poc?uri=deeplink:perfilContratante&idBp=p6cgeJ%2FgjC4QK2TEfXGy%2BA%3D%3D'
+p20
+sS'estado'
+p21
+VResuelta
+p22
+sS'desc_expediente'
+p23
+VActualización y garantía de las licencias de los productos Oracle del Imserso
+p24
+sS'importe'
+p25
+V177.921,38
+p26
+sb.
--- 10.contratacionE/scrapePCE.py
+++ 10.contratacionE/scrapePCE.py
@@ -4,54 +4,58 @@
 
 from bs4 import BeautifulSoup
 import sys
+import cPickle as pickle
 
 """ Clase que de cada linea de la tabla extraida de contratos
 obtiene los datos de cada contrato"""
 class Contrato:
 
     
-#    num_expediente = ""
-#    desc_expediente =""
-#    tipoContrato = []
-#    estado = ""
-#    importe = ""
-#    fechas = [] 
-#    organo = []
-
     def __init__(self, row):
 
-        cells = row.findAll("td")
-	    #print len(cells)
-	    #For each "tr", assign each "td" to a variable.
-        if len(cells) == 6:
-            expediente = cells[0].findAll(text=True)
-            self.num_expediente = expediente[0]
-            self.desc_expediente = expediente[1]
-	      #print(expediente)  
-            self.tipoContrato = cells[1].findAll(text=True)
-            self.estado = cells[2].find(text=True)
-            self.importe = cells[3].find(text=True)
-            #self.fechas = cells[4].findAll(text=True)
-            self.organo = cells[5].findAll(text=True)
-        
-        # Buscamos las fechas por la clase de columna
-        self.Fecha={}
-        fechas = row.find("td", {'class': 'tdFecha'})
-        diaFecha=""
-        for fecha in fechas.findAll('div'):
-            tipoFecha =fecha.find('span', {'class':'anchoTipoFecha'}).string
-            
-            # Busca la fecha
-            diaFecha= fecha.find('span', {'class':'textAlignLeft'})
-            if diaFecha is None:
-                diaFecha=""
-            else:
-                diaFecha=diaFecha.string
+        # Check that row has teh correct class
+        if row.get("class")[0] in ['rowClass1','rowClass2']:
 
+            # Buscamos los datos del expediente por la clase de columna tdExpediente
+            expediente = row.find("td", {'class': 'tdExpediente'}).findAll('div')    
+            self.num_expediente = expediente[0].text
+            self.desc_expediente = expediente[1].text
+
+            # Buscamos los datos del contrato por la clase de columna tdTipoContrato
+            self.tiposContrato=[]
+            for tipoContrato in row.find("td", {'class': 'tdTipoContrato'}).findAll('div'):
+                self.tiposContrato.append(tipoContrato.text)
+
+            # Buscamos el estado del expediente por la clase de columna tdEstado
+            self.estado = row.find("td", {'class': 'tdEstado'}).text
+ 
+            # Buscamos el importe del expediente por la clase de columna tdImporte
+            self.importe = row.find("td", {'class': 'tdImporte'}).text
             
-            self.Fecha[tipoFecha]=diaFecha
-#        print(fechas.prettify())
+            # Buscamos el organo de contratación por la clase de columna tdOrganoContratacion
+            datosOrganoC = row.find("td", {'class': 'tdOrganoContratacion'})
+            self.organo = datosOrganoC.text
+            self.organoURL=datosOrganoC.find('a').get('href')
+            
+            # Buscamos las fechas por la clase de columna tdFecha
+            self.Fecha={}
+            diaFecha=""
+            
+            for fecha in row.find("td", {'class': 'tdFecha'}).findAll('div'):
+                tipoFecha =fecha.find('span', {'class':'anchoTipoFecha'}).text
+                
+                # Busca la fecha
+                diaFecha= fecha.find('span', {'class':'textAlignLeft'})
+                if diaFecha is None:
+                    diaFecha=""
+                else:
+                    diaFecha=diaFecha.text
     
+                
+                self.Fecha[tipoFecha]=diaFecha
+    #        print(fechas.prettify())
+
+
     
     
     
@@ -62,10 +66,13 @@ def main():
     exp_data = open('tablaContratos_2.txt','r').read()
 
     soup = BeautifulSoup(exp_data)
-      
+     
+    # Envia sólo las líneas que son de contratos 
     for row in soup.findAll("tr",  {'class': ['rowClass1', 'rowClass2']}):
-        listaContratos.append(Contrato(row))
+        pickle.dump(Contrato(row),  open('save.p', 'ab')) 
+        #listaContratos.append(Contrato(row))
         #print(row.prettify())
+        
     
     
 if __name__ == "__main__":
